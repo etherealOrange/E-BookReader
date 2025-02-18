0288e0a2de44e6b74e9b688bb0322105586790da
hi world <2607233502@qq.com>
2025-02-18 16:53:30 +0800

    成功的构建了Android15的空项目，可以运行

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven { url =uri("https://maven.aliyun.com/repository/public") }
+        maven { url =uri("https://maven.aliyun.com/repository/google") }
+        maven { url =uri("https://maven.aliyun.com/repository/grails-core") }
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EBook-Reader"
+include(":app")
+ 
